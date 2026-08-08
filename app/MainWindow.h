db47afd8986b48959767a9abaaa9a07545ecf8c7
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-09 00:55:39 +0300

    Redesign the main preparation workflow

--- app/MainWindow.h
+++ app/MainWindow.h
@@ -4,27 +4,33 @@
 class QLabel;
 class MapperOverlay;
 class IntegratedView;
-class QProcess;
+class QPushButton;
+class QSpinBox;
 
 class MainWindow final : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
 private slots:
-    void refreshWaydroidStatus();
-    void startWaydroidSession();
-    void launchWaydroid();
-    void showWaydroid();
+    void prepareWaydroid();
     void stopWaydroid();
     void showIntegratedWaydroid();
     void showOverlay();
     void handleCapturedKey(int key, bool pressed);
+    void updateControls();
+
 private:
     void setStatus(const QString &text, bool healthy);
+
     QLabel *statusLabel_ = nullptr;
     QLabel *eventLabel_ = nullptr;
-    QProcess *statusProcess_ = nullptr;
+    QSpinBox *widthBox_ = nullptr;
+    QSpinBox *heightBox_ = nullptr;
+    QPushButton *prepareButton_ = nullptr;
+    QPushButton *stopButton_ = nullptr;
+    QPushButton *integratedButton_ = nullptr;
     MapperOverlay *overlay_ = nullptr;
     IntegratedView *integratedView_ = nullptr;
 };
